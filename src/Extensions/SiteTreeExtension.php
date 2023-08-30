05b2c5598da7a1a12a2d497620df370de6ca567b
Rastislav Brandobur <rastislav.brandobur@gmail.com>
2023-08-30 21:13:22 +0200

    added support for back link

--- src/Extensions/SiteTreeExtension.php
+++ src/Extensions/SiteTreeExtension.php
@@ -2,6 +2,8 @@
 
 namespace Rasstislav\IdSk\Extensions;
 
+use gorriecoe\Link\Models\Link;
+use gorriecoe\LinkField\LinkField;
 use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\ORM\DataExtension;
@@ -12,12 +14,33 @@ class SiteTreeExtension extends DataExtension
         'ShowInFooter' => 'Boolean',
     ];
 
+    private static $has_one = [
+        'BackLink' => Link::class,
+    ];
+
     private static $field_labels = [
         'ShowInFooter' => 'Zobraziť v pätičke?',
+        'BackLink' => 'Tlačidlo späť',
     ];
 
     public function updateSettingsFields(FieldList $fields)
     {
-        $fields->insertAfter('ShowInMenus', new CheckboxField('ShowInFooter', $this->owner->fieldLabel('ShowInFooter')));
+        $fields->insertAfter('ShowInMenus', CheckboxField::create('ShowInFooter', $this->owner->fieldLabel('ShowInFooter')));
+
+        if (class_exists(LinkField::class)) {
+            $fields->insertAfter('Visibility',
+                LinkField::create(
+                    'BackLink',
+                    $this->owner->fieldLabel('BackLink'),
+                    $this->owner,
+                    [
+                        'types' => [
+                            'SiteTree',
+                            'URL',
+                        ],
+                    ],
+                )
+            );
+        }
     }
 }
