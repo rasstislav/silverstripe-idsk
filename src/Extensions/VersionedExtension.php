8791863a555c8e5c68116f4a9e653c270d90a40b
Rastislav Brandobur <rastislav.brandobur@gmail.com>
2023-09-26 09:47:16 +0200

    fixed PublishedUpdated flag with fluent

--- src/Extensions/VersionedExtension.php
+++ src/Extensions/VersionedExtension.php
@@ -4,13 +4,27 @@ namespace Rasstislav\IdSk\Extensions;
 
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\Versioned\Versioned;
+use TractorCow\Fluent\State\FluentState;
 
 class VersionedExtension extends DataExtension
 {
     public function isFirstPublishedVersion()
     {
-        return $this->owner->hasExtension(Versioned::class)
-            && $this->owner->Versions('"WasPublished" = 1')->count() === 1;
+        if ($this->owner->hasExtension(Versioned::class)) {
+            $callback = fn() => $this->owner->Versions('"WasPublished" = 1')->count() === 1;
+
+            if (class_exists(FluentState::class)) {
+                return FluentState::singleton()->withState(function (FluentState $state) use ($callback): bool {
+                    $state->setLocale(null);
+
+                    return $callback();
+                });
+            } else {
+                return $callback();
+            }
+        }
+
+        return false;
     }
 
     public function PublishedUpdated()
