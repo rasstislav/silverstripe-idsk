9a121b3fa7d493bf3aa6025e025cb9b2bf8f660b
Rastislav Brandobur <rastislav.brandobur@gmail.com>
2023-10-05 22:49:55 +0200

    added support for uploading logo to header and footer

--- src/Extensions/SiteConfigExtension.php
+++ src/Extensions/SiteConfigExtension.php
@@ -3,8 +3,11 @@
 namespace Rasstislav\IdSk\Extensions;
 
 use Rasstislav\IdSk\TinyMCEConfig;
+use SilverStripe\Assets\Image;
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Forms\FormScaffolder;
 use SilverStripe\Forms\Tab;
+use SilverStripe\Forms\TabSet;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TreeDropdownField;
 use SilverStripe\ORM\DataExtension;
@@ -23,10 +26,19 @@ class SiteConfigExtension extends DataExtension
     ];
 
     private static $has_one = [
+        'HeaderLogo' => Image::class,
+        'FooterLogo' => Image::class,
         'CookiesPage' => SiteTree::class,
     ];
 
+    private static $owns = [
+        'HeaderLogo',
+        'FooterLogo',
+    ];
+
     private static $field_labels = [
+        'HeaderLogo' => 'Logo',
+        'FooterLogo' => 'Logo',
         'CookiesBanner' => 'Povoliť cookies a zobrazenie cookies bannera',
         'CookiesBannerHeading' => 'Nadpis',
         'CookiesBannerText' => 'Obsah',
@@ -39,28 +51,39 @@ class SiteConfigExtension extends DataExtension
     ];
 
     public function updateCMSFields(FieldList $fields) {
-        // TODO: optional unclecheese/silverstripe-display-logic
-        $fields->insertBefore(Tab::create('Cookies', 'Cookies',
-            $this->owner->dbObject('CookiesBanner')->scaffoldFormField()
-                ->setTitle($this->owner->fieldLabel('CookiesBanner')),
-            $this->owner->dbObject('CookiesBannerHeading')->scaffoldFormField()
-                ->setTitle($this->owner->fieldLabel('CookiesBannerHeading')),
-            $cookiesBannerTextField = $this->owner->dbObject('CookiesBannerText')->scaffoldFormField()
-                ->setTitle($this->owner->fieldLabel('CookiesBannerText'))
-                ->setRows(3),
-            $this->owner->dbObject('CookiesBannerButtons')->scaffoldFormField()
-                ->setTitle($this->owner->fieldLabel('CookiesBannerButtons')),
-            $this->owner->dbObject('CookiesBannerAcceptButtonTitle')->scaffoldFormField()
-                ->setTitle($this->owner->fieldLabel('CookiesBannerAcceptButtonTitle')),
-            $this->owner->dbObject('CookiesBannerRejectButtonTitle')->scaffoldFormField()
-                ->setTitle($this->owner->fieldLabel('CookiesBannerRejectButtonTitle')),
-            $cookiesBannerAcceptedField = $this->owner->dbObject('CookiesBannerAccepted')->scaffoldFormField()
-                ->setTitle($this->owner->fieldLabel('CookiesBannerAccepted'))
-                ->setRows(3),
-            $cookiesBannerRejectedField = $this->owner->dbObject('CookiesBannerRejected')->scaffoldFormField()
-                ->setTitle($this->owner->fieldLabel('CookiesBannerRejected'))
-                ->setRows(3),
-            TreeDropdownField::create('CookiesPageID', $this->owner->fieldLabel('CookiesPage'), SiteTree::class),
+        $fields->insertBefore(TabSet::create('Header', 'Hlavička',
+            Tab::create('Main', _t(FormScaffolder::class . '.TABMAIN', 'Main'),
+                $this->owner->dbObject('HeaderLogoID')->scaffoldFormField()
+                    ->setTitle($this->owner->fieldLabel('HeaderLogo')),
+            ),
+            // TODO: optional unclecheese/silverstripe-display-logic
+            Tab::create('Cookies', 'Cookies',
+                $this->owner->dbObject('CookiesBanner')->scaffoldFormField()
+                    ->setTitle($this->owner->fieldLabel('CookiesBanner')),
+                $this->owner->dbObject('CookiesBannerHeading')->scaffoldFormField()
+                    ->setTitle($this->owner->fieldLabel('CookiesBannerHeading')),
+                $cookiesBannerTextField = $this->owner->dbObject('CookiesBannerText')->scaffoldFormField()
+                    ->setTitle($this->owner->fieldLabel('CookiesBannerText'))
+                    ->setRows(3),
+                $this->owner->dbObject('CookiesBannerButtons')->scaffoldFormField()
+                    ->setTitle($this->owner->fieldLabel('CookiesBannerButtons')),
+                $this->owner->dbObject('CookiesBannerAcceptButtonTitle')->scaffoldFormField()
+                    ->setTitle($this->owner->fieldLabel('CookiesBannerAcceptButtonTitle')),
+                $this->owner->dbObject('CookiesBannerRejectButtonTitle')->scaffoldFormField()
+                    ->setTitle($this->owner->fieldLabel('CookiesBannerRejectButtonTitle')),
+                $cookiesBannerAcceptedField = $this->owner->dbObject('CookiesBannerAccepted')->scaffoldFormField()
+                    ->setTitle($this->owner->fieldLabel('CookiesBannerAccepted'))
+                    ->setRows(3),
+                $cookiesBannerRejectedField = $this->owner->dbObject('CookiesBannerRejected')->scaffoldFormField()
+                    ->setTitle($this->owner->fieldLabel('CookiesBannerRejected'))
+                    ->setRows(3),
+                TreeDropdownField::create('CookiesPageID', $this->owner->fieldLabel('CookiesPage'), SiteTree::class),
+            )
+        ), 'Access');
+
+        $fields->insertBefore(Tab::create('Footer', 'Pätička',
+            $this->owner->dbObject('FooterLogoID')->scaffoldFormField()
+                ->setTitle($this->owner->fieldLabel('FooterLogo')),
         ), 'Access');
 
         $tinyMCEConfig = TinyMCEConfig::get('cms');
